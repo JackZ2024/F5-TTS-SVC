c3010f2ade6a115ce4b040db77324d92ea312a73
Jack <jack20240612@outlook.com>
2025-05-02 13:30:00 +0900

    调整依赖安装顺序，解决冲突导致的安装依赖失败的问题。

--- setup.bat
+++ setup.bat
@@ -3,6 +3,8 @@
 set PATH=%CD%\runtime\Scripts\;PATH=%CD%\runtime\;%PATH%
 runtime\python.exe -m pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu121
 runtime\python.exe -m pip install -r requirements.txt
-runtime\python.exe -m pip install gdown
+REM runtime\python.exe -m pip install gdown
+runtime\python.exe -m pip install fairseq
+
 echo 安装包完成。
 pause
